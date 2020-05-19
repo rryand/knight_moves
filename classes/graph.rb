d7149662bd1eb36fbc83bbc57939a11a1a74b262
Ramses Ryan Dineros <ramsesryandineros@gmail.com>
2020-05-20 00:54:32 +0800

    finished trace_route using breadth-first traversal

--- classes/graph.rb
+++ classes/graph.rb
@@ -8,10 +8,14 @@ class Graph
   public
 
   def build_graph
-    puts "building graph..."
     build_nodes
     build_edges
-    #@nodes[[0, 0]].adjacent_nodes.each { |n| p n.pos }
+  end
+
+  def trace_route(initial, final)
+    queue = [initial]
+    parent = bfs(queue, final)
+    backtrace(parent, initial, final)
   end
 
   private
@@ -26,12 +30,10 @@ class Graph
 
   def build_edges
     @nodes.each_key do |x, y|
-      p [x, y]
       [[x + 1, y + 2], [x + 2, y + 1],
        [x + 2, y - 1], [x + 1, y - 2],
        [x - 1, y - 2], [x - 2, y - 1],
        [x - 2, y + 1], [x - 1, y + 2]].each do |x2, y2|
-        puts "  -#{[x2, y2]}" if edge_is_valid?([x, y], [x2, y2])
         add_edge([x, y], [x2, y2]) if edge_is_valid?([x, y], [x2, y2])
        end
     end
@@ -46,5 +48,27 @@ class Graph
     x2, y2 = pos2
     x2.between?(0, 7) && y2.between?(0, 7) && 
     !@nodes[pos1].adjacent_nodes.include?(pos2)
+  end
+
+  def bfs(queue, final, moves = [], parent = {}) # breadth-first traversal
+    pos = queue.shift
+    moves << pos
+    return parent if pos == final
+    @nodes[pos].adjacent_nodes.each do |child_node|
+      unless moves.include?(child_node.pos)
+        queue << child_node.pos
+        parent[child_node.pos] = pos
+      end
+    end
+    bfs(queue, final, moves, parent)
+  end
+
+  def backtrace(parent, initial, final) # use parent hash to trace path
+    return initial if parent.empty?
+    path = [final]
+    while path[-1] != initial
+      path << parent[path[-1]]
+    end
+    path.reverse
   end
 end
--- main.rb
+++ main.rb
@@ -1,8 +1,15 @@
 require_relative "classes/graph"
 
-def knight_moves
+puts "\e[1mKnight moves:\e[0m"
+
+def knight_moves(initial, final)
   graph = Graph.new
   graph.build_graph
+  puts "#{initial} -> #{final}:"
+  puts " -#{graph.trace_route(initial, final)}"
 end
 
-knight_moves
+knight_moves([0,0],[1,2])
+knight_moves([0,0],[3,3])
+knight_moves([3,3],[0,0])
+knight_moves([0,0],[1,1])
